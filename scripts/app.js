4a655f1c5dcd2169336cbe100e040c7a0e3f59bb
PurityGwaro <puritygwaro@gmail.com>
2022-05-06 22:06:50 +0300

    added editing of UI upon fetch of weather condition and city keys

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,60 @@
 //for DOM manipulation
+const cityForm = document.querySelector('form');
+const card =  document.querySelector('.card')
+const details = document.querySelector('.details');
 
+
+const updateUI = (data) =>{
+
+    const cityDetails = data.cityDetails;
+    const weather = data.weather;
+
+    //update details template
+    details.innerHTML = `
+        <h5 class="my-3">${cityDetails.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Metric.Value}</span><!--shows the temp in no form-->
+            <span>&deg;C</span><!--gets us the little circle for the degree sign-->
+        </div>
+    `;
+    //remove the d-none class
+    
+
+
+};
+
+const updateCity = async (city) => {
+    //make a call to getCity to get city key
+    //then call getWeather and use the city key to get the condition
+    //console.log(city);
+
+    const cityDetails = await getCity(city);
+    const weather = await getWeather(cityDetails.Key);
+
+    return {
+        //cityDetails: cityDetails,
+        //weather: weather
+        //use object short hand notation when the key and value both have same names: you delete one of them
+        cityDetails,
+        weather
+    };
+
+};
+
+cityForm.addEventListener('submit', e => {
+    //prevent default action
+    e.preventDefault();
+
+    //get the city the user types in
+    const city = cityForm.city.value.trim();
+    cityForm.reset();//clears out the form fills after submition
+    //console.log(city);
+
+    //update the UI with the new city
+    updateCity(city)
+        .then(data => updateUI(data))
+        .catch(err => console.log(err));
+    ;
+
+});
--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,4 +1,6 @@
 //interaction with the weather API
+//query-parameters: API key and city name
+
 
 //create an app in the AccuWeather APIs website in order to get an API key from the API service
 //the key identifies who's making the req and what app is making it to the API service
@@ -13,8 +15,46 @@ const key = 'O2BbRGdH3iDtU7jx0d92IFi1GKtYOSrB';//API key for AccuWeather
 //then use the city code on the current conditions API to get its condition
 //city code is sent to the current conditions endpoint to get info on it
 
-const getCity = async () => {//returns a promise
+//req1. get weather info
+const getWeather = async (id) => {//id rep the city's key
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
 
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';//url for city search
+    const reponse = await fetch(base + query);
+    const data = await reponse.json();
+
+    //console.log(data);
+    return data[0];
 
 };
+
+
+//req2. get city info
+const getCity = async (city) => {//returns a promise
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';//url for city search
+    const query = `?apikey=${key}&q=${city}`;//adding query parameters to the end of a url
+    //combine the base key with the query parameters and fetch the resource
+
+    const reponse = await fetch(base + query);
+    const data = await reponse.json();
+
+    //console.log(data);//returns an array with possible matches... the first value is the closest
+    //console.log(data[0]);
+    return data[0];//returns a promise. Hence when calling it we have to tack on the .then method
+
+};
+/*
+getCity('kisumu')
+    //.then(data => console.log(data))
+    .then(data => {
+       return getWeather(data.Key); 
+    }).then(data => {
+        console.log(data);
+    })
+    .catch(err => console.log(err)); 
+      
+;
+
+//getWeather('224758');
+*/
